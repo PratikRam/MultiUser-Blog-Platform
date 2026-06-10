3c31ee0eedb90d8db8e5ebe77fdd1c66749b069d
rampratik <rampratik985@gmail.com>
2026-06-10 17:26:59 +0530

    add render URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://multiuser-blog-platform.onrender.com/api",
+    // baseURL : "http://localhost:8080/api",
     headers: {
         "Content-Type": "application/json",
     },
